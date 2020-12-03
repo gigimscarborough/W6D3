fe3cc0e3630aff7190835cf8aae616756dd8c92e
junghyun2006 <jung.hyun.park516@gmail.com>
2020-12-03 15:05:17 -0500

    Finish Nested Postman

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
     def index
         render plain: "I'm in the index action!"
     end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params[:id]
+    end
+
+    {"some_category"=>{"a_key"=>"another value", 
+    "a_second_key"=>"yet another value",
+     "inner_inner_hash"=>{"key"=>"value"}},
+      "something_else"=>"aaahhhhh"}
 end
